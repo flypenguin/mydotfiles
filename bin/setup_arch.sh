603bdf7bd9d23f4f125b6f31e8e0e735c00f1d38
Axel Bock <mr.axel.bock@gmail.com>
2015-11-17 15:45:46 +0100

    added arch setup script

---
+++ bin/setup_arch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+# set up arch linux systems
+
+
+sudo yaourt -S freetype2-infinality fontconfig-infinality
+sudo pacman -S python2-pip python-pip python-virtualenvwrapper docker docker-compose jq zsh
+
+chsh /usr/bin/zsh
