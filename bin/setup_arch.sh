ff4960e7e76c7312f94f5b870e0a29860b9ffa1d
Axel Bock <mr.axel.bock@gmail.com>
2015-11-17 15:50:00 +0100

    updated arch setup

--- bin/setup_arch.sh
+++ bin/setup_arch.sh
@@ -3,7 +3,7 @@
 # set up arch linux systems
 
 
-sudo yaourt -S freetype2-infinality fontconfig-infinality
-sudo pacman -S python2-pip python-pip python-virtualenvwrapper docker docker-compose jq zsh
+yaourt -S freetype2-infinality fontconfig-infinality
+sudo pacman -S python2-pip python-pip python-virtualenvwrapper docker docker-compose jq zsh terminator
 
-chsh /usr/bin/zsh
+chsh -s /usr/bin/zsh
