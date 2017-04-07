4e1ddfbbdc6defc73c860cd553f74fc0dd90a8d0
Axel Bock <mr.axel.bock@gmail.com>
2017-04-07 23:08:33 +0200

    add weird workaround

--- .shell/rvm.zsh
+++ .shell/rvm.zsh
@@ -1,6 +1,11 @@
 # unfortunately, the rvm plugin should be loaded here. meh.
 # load rvm
 if [ -d "$HOME/.rvm" ]; then
-    path=("$HOME/.rvm" $path)
+    # this is a WEIRD zgen workaround, at least on Mac. super weird.
+    # it does actually seem that the PATH variable is unset here.
+    # WTF??
+    BPATH=$PATH
+    export PATH="/usr/local/bin:/usr/bin:/bin"
     [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+    export PATH="$BPATH"
 fi
