ba678a7dc554a92efa72e0304d7f024edf63e817
Axel Bock <mr.axel.bock@gmail.com>
2017-09-09 15:11:55 +0200

    use stow now for dotfile management

---
+++ dotfiles/bin/setup_generic.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# $1 repo URL to be cloned
+# $2 target directory, must be set
+clone_repo_to() {
+  if ! [ -d "$2" ] ; then
+    echo "   * Cloning $1 -> $2 ..."
+    mkdir -p "$(dirname $2)"
+    git clone "$1" "$2"
+  else
+    echo "   * $2 already present, skipping '$1'"
+  fi
+}
+# install rvm - ruby version manager
+if [ ! -d "$HOME/.rvm" ] ; then
+  echo " * Setting up rvm ..."
+  gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+  \curl -sSL https://get.rvm.io | bash -s stable
+else
+  echo " * rvm seems to be installed. please update using rvm itself."
+fi
+
+# install zplug - zsh plugin manager
+echo " * Installing zsh manager(s) ..."
+clone_repo_to https://github.com/tarjoilija/zgen.git          "${HOME}/.zgen"
+#clone_repo_to https://github.com/zplug/zplug                  "$HOME/.zplug"
+#clone_repo_to https://github.com/robbyrussell/oh-my-zsh.git   "$HOME/.oh-my-zsh"
+
+# now use stow to manage shit. :)
+# first, save what might be overwritten.
+exec "$HOME/.dotfiles/dotfiles/bin/dotfile-update" -f
---
+++ dotfiles/bin/setup_mac.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# install homebrew if needed
+if ! which brew ; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew tap caskroom/bundle
+  brew bundle                 # so much nicer :)
+  brew bundle                 # so much nicer :)
+else
+  echo "Skipping homebrew installation. Seems to be installed."
+fi
+
+# clone the repo if not yet done ;)
+if [ ! -d "$HOME/.dotfiles" ] ; then
+  cd "$HOME"
+  # now we *definitively* have git ...
+  git clone https://github.com/flypenguin/mydotfiles.git .dotfiles
+fi
+
+# continue with generic items
+exec "$HOME/.dotfiles/dotfiles/bin/setup_generic.sh"
