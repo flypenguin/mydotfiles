e265243e01327079a47be4ef76f2e09983c4a62f
Axel Bock <mr.axel.bock@gmail.com>
2017-09-13 19:54:48 +0200

    update for arch linux installation

--- setup/arch.sh
+++ setup/arch.sh
@@ -5,88 +5,97 @@
 SUDO_STRING="%wheel ALL=(ALL) NOPASSWD: ALL"
 SUDO_MATCHR='^%wheel ALL=\(ALL\) NOPASSWD: ALL$'
 WANT_SHELL="/usr/bin/zsh"
-YAOURT_PACKAGES="freetype2-infinality fontconfig-infinality sublime-text-dev"
-PACMAN_PACKAGES="python2-pip python-pip python-virtualenvwrapper docker docker-compose jq zsh terminator vim rsync"
+YAOURT_PACKAGES=""
+PACMAN_PACKAGES="stow python2-pip python-pip python-virtualenvwrapper docker docker-compose jq zsh terminator vim rsync"
 
 
 # ============================================================================
 echo -e "\n\n Installing sudo rules ... \n"
 
 if ! sudo cat /etc/sudoers | egrep "${SUDO_MATCHR}" > /dev/null ; then
-    echo "Adding global sudo rule ..."
+    echo " * Adding global sudo rule ..."
     sudo bash -c 'echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
     sudo rm -rf "/etc/sudoers.d/"*
 else
-    echo "Skipping sudo rule installation, already present."
+    echo " * Skipping sudo rule installation, already present."
 fi
 
 
 # ============================================================================
-echo -e "\n\n Installing packages ... \n"
+echo -e "\n\n * Installing packages ... \n"
 
 for p in $YAOURT_PACKAGES ; do
     if ! pacman -Qs $p > /dev/null ; then
-        yaourt -S $p
+        echo "   * Yaourt: installing $p"
+        yaourt -S --noconfirm $p
     else
-        echo "Yaourt: $p already installed."
+        echo "   * Yaourt: package already installed: $p"
     fi
 done
 
 for p in $PACMAN_PACKAGES ; do
     if ! pacman -Qs $p > /dev/null ; then
-        pacman -S $p
+        echo "   * Pacman: installing $p"
+        sudo pacman -S --noconfirm $p
     else
-        echo "Pacman: $p already installed."
+        echo "   * Pacman: package already installed: $p"
     fi
 done
 
 
 # ============================================================================
-echo -e "\n\n Setting shell ... \n"
+echo -e "\n\n * Setting shell ... \n"
 
 if [ ! "$(getent passwd $USER | cut -d: -f7)" = "$WANT_SHELL" ] ; then
-    echo "Changing shell to $WANT_SHELL ..."
+    echo "   * Changing shell to $WANT_SHELL ..."
     chsh -s /usr/bin/zsh
 else
-    echo "Shell already set to $WANT_SHELL"
+    echo "   * Shell already set to $WANT_SHELL"
 fi
 
 
 # ============================================================================
-echo -e "\n\n Setting udpating dotfiles ... \n"
-
-echo "Update dotfiles ..."
-$(dirname $0)/update_dotfiles.sh git2home
-
-
-# ============================================================================
-echo -e "\n\n Modifying user's groups ... \n"
-
-echo "Adding docker group ..."
+echo "\n\n * Adding this user to the docker group ..."
 sudo gpasswd -a $(id -un) docker
 
 
 # ============================================================================
-echo -e "\n\n Enabling system services ... \n"
+echo -e "\n\n * Enabling system services ... \n"
 
-echo "Enabling Docker service ..."
-sudo systemctl enable docker
-sudo systemctl start docker
-
-echo "Enabling SSH service ..."
-sudo systemctl enable sshd
-sudo systemctl start sshd
-
-echo "Setting up rest ..."
-"$(dirname $0)/generic.sh"
+for service in docker sshd ; do 
+  echo "   * enable & start service '$service'..."
+  sudo systemctl enable $service
+  sudo systemctl start $service
+done
 
 
 # ============================================================================
-echo -e "\n\n Installing linuxbrew ... \n"
+echo -e "\n\n * Installing linuxbrew ... \n"
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+if ! which brew > /dev/null 2>&1 ; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+else
+  echo "   * Linuxbrew seems to be installed already. Skipping."
+fi
+
+
+# ============================================================================
+# clone the repo if not yet done ;)
+echo -e "\n\n * Cloning dotfiles repo ..."
+if [ ! -d "$HOME/.dotfiles" ] ; then
+  cd "$HOME"
+  # now we *definitively* have git ...
+  git clone https://github.com/flypenguin/mydotfiles.git .dotfiles
+else
+  echo "   * Seems to be present already. Skipping."
+fi
+
+
+# ============================================================================
+echo -e "\n\n * Generic Linux setup ..."
+"$HOME/.dotfiles/setup/generic.sh"
 
 
 # ============================================================================
 echo ""
-echo -e "\n\n Done.\n Please re-login. \n"
+echo -e "\n\nDone.\nPlease re-login. \n"
--- setup/generic.sh
+++ setup/generic.sh
@@ -15,24 +15,32 @@ clone_repo_to() {
     echo "   * $2 already present, skipping '$1'"
   fi
 }
+
+
+## START
+
 # install rvm - ruby version manager
 if [ ! -d "$HOME/.rvm" ] ; then
-  echo " * Setting up rvm ..."
+  echo -e "\n\n * Installing up rvm ..."
   gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
   \curl -sSL https://get.rvm.io | bash -s stable
 else
-  echo " * rvm seems to be installed. please update using rvm itself."
+  echo "   * rvm seems to be installed. please update using rvm itself."
 fi
 
+
 # install zplug - zsh plugin manager
-echo " * Installing zsh manager(s) ..."
+echo -e "\n\n * Installing zsh manager(s) ..."
 clone_repo_to https://github.com/tarjoilija/zgen.git          "${HOME}/.zgen"
 #clone_repo_to https://github.com/zplug/zplug                  "$HOME/.zplug"
 #clone_repo_to https://github.com/robbyrussell/oh-my-zsh.git   "$HOME/.oh-my-zsh"
 
+
 # now use stow to manage shit. :)
 # first, save what might be overwritten.
+echo -e "\n\n * Updating dotfiles ..."
 "$HOME/.dotfiles/dotfiles/bin/dotfile-update" -f
+
 
 echo -e "\n\nIMPORTANT NOTE\nPlease remember to switch your shell to zsh :)\n"
 echo -e "\n... and have fun :)\n\n"
