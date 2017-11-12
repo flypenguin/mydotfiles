a3bb28b68b43b40ac97fdc8725d2632bbf3243d4
Axel Bock <mr.axel.bock@gmail.com>
2017-11-12 03:44:24 +0100

    add direnv to helpers.sh

--- dotfiles/.shell/helpers.sh
+++ dotfiles/.shell/helpers.sh
@@ -1,2 +1,5 @@
 # autojump
 [ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
+
+# direnv - https://direnv.net/
+which -s direnv > /dev/null 2>&1 && eval "$(direnv hook zsh)"
