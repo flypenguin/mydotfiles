51ed79f988aff51efd3bcf360acf87ed15f89234
Axel Bock <mr.axel.bock@gmail.com>
2018-01-16 17:57:03 +0100

    fix arch.sh setup script even more

--- setup/arch.sh
+++ setup/arch.sh
@@ -6,18 +6,28 @@ SUDO_STRING="%wheel ALL=(ALL) NOPASSWD: ALL"
 SUDO_MATCHR='^%wheel ALL=\(ALL\) NOPASSWD: ALL$'
 WANT_SHELL="/usr/bin/zsh"
 YAOURT_PACKAGES=""
-PACMAN_PACKAGES="stow python2-pip python-pip python-virtualenvwrapper docker docker-compose jq zsh vim rsync base-devel"
+PACMAN_PACKAGES="stow python2-pip python-pip python-virtualenvwrapper docker docker-compose jq zsh vim rsync base-devel openssh ruby"
+LOGFILE="/tmp/setup-arch.$(date +%Y%m%d_%H%M%S).log"
+
 
 die() {
     echo "$@"
     exit -1
 }
 
+runme() {
+    echo -e "COMMAND: $@\n\n" >> $LOGFILE
+    "$@" >> $LOGFILE 2>&1
+    echo -e "\n\n" >> $LOGFILE
+}
+
+
 # ============================================================================
 echo -e "\n\n Some preliminary checks ...\n"
 
 set -euo pipefail
 
+echo -n " * LOGFILE IS: $LOGFILE"
 echo -n " * checking if $WANT_SHELL exists ... "
 test -x "$WANT_SHELL" && echo "ok" || die "XX"
 
@@ -25,9 +35,9 @@ test -x "$WANT_SHELL" && echo "ok" || die "XX"
 echo -e "\n\n Installing pacman packages ...\n"
 
 for p in $PACMAN_PACKAGES ; do
-    if ! pacman -Qs $p > /dev/null ; then
+    if ! pacman -Q $p | cut -d" " -f 1 | grep -qE "^$p$" ; then
         echo "   * Pacman: installing $p"
-        sudo pacman -S --noconfirm $p
+        runme sudo pacman -S --noconfirm $p
     else
         echo "   * Pacman: package already installed: $p"
     fi
@@ -40,11 +50,11 @@ if ! which yaourt >/dev/null 2>&1 ; then
     TMP=$(mktemp -d)
     cd "$TMP"
     for pkg in package-query yaourt ; do
-        curl -O "https://aur.archlinux.org/cgit/aur.git/snapshot/${pkg}.tar.gz"
-        tar xzf "${pkg}.tar.gz"
-        cd "${pkg}"
-        makepkg -si
-        cd ..
+        runme curl -sLO "https://aur.archlinux.org/cgit/aur.git/snapshot/${pkg}.tar.gz"
+        runme tar xzf "${pkg}.tar.gz"
+        runme cd "${pkg}"
+        runme makepkg --noconfirm -si
+        runme cd ..
     done
 else
     echo " * Skipping yaourt installation, seems to be already present."
@@ -55,20 +65,20 @@ echo -e "\n\n Installing sudo rules ... \n"
 
 if ! sudo cat /etc/sudoers | egrep "${SUDO_MATCHR}" > /dev/null ; then
     echo " * Adding global sudo rule ..."
-    sudo bash -c 'echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
-    sudo rm -rf "/etc/sudoers.d/"*
+    runme sudo bash -c 'echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers'
+    runme sudo rm -rf "/etc/sudoers.d/"*
 else
     echo " * Skipping sudo rule installation, already present."
 fi
 
 
 # ============================================================================
-echo -e "\n\n * Installing packages ... \n"
+echo -e "\n\n * Installing yaourt packages ... \n"
 
 for p in $YAOURT_PACKAGES ; do
     if ! pacman -Qs $p > /dev/null ; then
         echo "   * Yaourt: installing $p"
-        yaourt -S --noconfirm $p
+        runme yaourt -S --noconfirm $p
     else
         echo "   * Yaourt: package already installed: $p"
     fi
@@ -88,7 +98,7 @@ fi
 
 # ============================================================================
 echo "\n\n * Adding this user to the docker group ..."
-sudo gpasswd -a $(id -un) docker
+runme sudo gpasswd -a $(id -un) docker
 
 
 # ============================================================================
@@ -96,8 +106,8 @@ echo -e "\n\n * Enabling system services ... \n"
 
 for service in docker sshd ; do 
   echo "   * enable & start service '$service'..."
-  sudo systemctl enable $service
-  sudo systemctl start $service
+  runme sudo systemctl enable $service
+  runme sudo systemctl start $service
 done
 
 
@@ -117,7 +127,7 @@ echo -e "\n\n * Cloning dotfiles repo ..."
 if [ ! -d "$HOME/.dotfiles" ] ; then
   cd "$HOME"
   # now we *definitively* have git ...
-  git clone https://github.com/flypenguin/mydotfiles.git .dotfiles
+  runme git clone https://github.com/flypenguin/mydotfiles.git .dotfiles
 else
   echo "   * Seems to be present already. Skipping."
 fi
