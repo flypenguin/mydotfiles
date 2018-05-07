ce4adbfca2a3660dfb63b817c1c77da6b42b5fa8
Axel Bock <mr.axel.bock@gmail.com>
2018-05-07 12:08:07 +0200

    add snaps to path management

--- dotfiles/.shell/cli-paths.sh
+++ dotfiles/.shell/cli-paths.sh
@@ -1,6 +1,17 @@
+# $HOME/clis
+
 if [ -d "$HOME/clis" ] ; then
   for p in "$HOME/clis/"* ; do
     [ -d "$p/bin" ] && p="$p/bin"
     path=("$p" "$path[@]")
   done
 fi
+
+# snaps :)
+
+for SNAP in "/var/lib/snapd/snap/bin" "/snap/bin" ; do
+  if [ -d "$SNAP" ]; then
+    path=("$path[@]" "$SNAP")
+    break
+  fi
+done
