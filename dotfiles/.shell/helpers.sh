235e25d6fde28ec7089ab8b5dde25e9f3d38b818
Axel Bock <mr.axel.bock@gmail.com>
2019-09-07 14:54:46 +0200

    add pkd shell function 'pkd-site-of'

--- dotfiles/.shell/helpers.sh
+++ dotfiles/.shell/helpers.sh
@@ -7,3 +7,7 @@ which -s direnv > /dev/null 2>&1 && eval "$(direnv hook zsh)"
 # nvm - the Node Version Manager
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+
+
+# PKD
+pkd-site-of() { okta-cli users list -s "profile.pkUsername eq \"$1\"" --output-fields profile.site }
