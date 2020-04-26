be6afd6516e96aaccd5514bb8e5aff87ac32f12f
Axel Bock <mr.axel.bock@gmail.com>
2020-04-26 11:10:57 +0200

    remove zsh-syntax-highlighting file

--- dotfiles/.shell/helpers.sh
+++ dotfiles/.shell/helpers.sh
@@ -9,3 +9,9 @@ which -s direnv > /dev/null 2>&1 && eval "$(direnv hook zsh)"
 
 # PKD
 pkd-site-of() { okta-cli users list -s "profile.pkUsername eq \"$1\"" --output-fields profile.site }
+
+# zsh syntax highlighting
+# from arch :)
+local SCRIPT="/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+[ -f "$SCRIPT" ] && source "$SCRIPT"
+
--- dotfiles/.shell/zsh-syntax-highlighting.sh
+++
@@ -1,6 +0,0 @@
-# zsh syntax highlighting
-# from arch :)
-
-local SCRIPT="/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-[ -f "$SCRIPT" ] && source "$SCRIPT"
-
