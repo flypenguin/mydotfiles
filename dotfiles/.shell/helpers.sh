0afd7e75e0a3d877bd6f692e98987f389f208e0a
Axel Bock <mr.axel.bock@gmail.com>
2020-04-26 11:12:28 +0200

    move work-related things into work.sh

--- dotfiles/.shell/helpers.sh
+++ dotfiles/.shell/helpers.sh
@@ -7,9 +7,6 @@ which -s direnv > /dev/null 2>&1 && eval "$(direnv hook zsh)"
 # nvm - the Node Version Manager
 # use the oh-my-zsh "nvm" plugin for "stupid" initialization, or use lazy loading here.
 
-# PKD
-pkd-site-of() { okta-cli users list -s "profile.pkUsername eq \"$1\"" --output-fields profile.site }
-
 # zsh syntax highlighting
 # from arch :)
 local SCRIPT="/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
