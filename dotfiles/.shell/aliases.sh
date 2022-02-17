3d382c94b6705ad19419aff58ac8f6d2f5f5bead
Axel Bock <mr.axel.bock@gmail.com>
2022-02-17 15:41:01 +0100

    add aliases: glv, glvc

--- dotfiles/.shell/aliases.sh
+++ dotfiles/.shell/aliases.sh
@@ -43,7 +43,9 @@ alias gdc="git diff --cached"
 # (g)it (d)iff ...
 #   (), (s)tat
 alias gls="git --no-pager log -n 40 --pretty=format:'%C(yellow)%h %ad%Creset %s %Cblue[%cn]%Cred%d%Creset' --decorate --date=short"
+alias glv="git --no-pager log -n 30 --pretty=format:'%C(yellow)%h %ad%Creset %s %Cblue[%cn]%Cred%d%Creset%+b' --decorate --date=short"
 alias glc="git            log       --pretty=format:'%C(yellow)%h %ad%Creset %s %Cblue[%cn]%Cred%d%Creset' --decorate --date=short"
+alias glvc="git            log       --pretty=format:'%C(yellow)%h %ad%Creset %s %Cblue[%cn]%Cred%d%Creset%+b' --decorate --date=short"
 alias gll="git log --pretty=format:'%C(yellow)%h %C(yellow reverse)%aD%Creset %s %Cblue[%cn]%Cred%d%Creset' --decorate --numstat"
 alias gld="git log --pretty=format:'%C(yellow reverse)%h%Creset %C(yellow reverse)%ad%Creset %C(white reverse)%s%Creset %Cblue[%cn]%C(red reverse)% d%Creset' --decorate --date=short -p"
 alias gds="git --no-pager diff --stat"
