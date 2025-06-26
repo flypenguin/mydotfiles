d349b159b717c5de3e80b3862c0a72cb08bd92b2
Axel Bock <ab@a3b3.de>
2025-06-26 12:45:30 +0200

    upd: nvim config - always use block cursor

--- dotfiles/.config/nvim/init.lua
+++ dotfiles/.config/nvim/init.lua
@@ -1,2 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- _always_ use block cursor, this stupid line is not visible ...
+vim.opt.guicursor = "n-v-i-c:block-Cursor"
